459953cf794a9d6695353abd3fb59d97a7c5becc
lieuzhenghong <lieuzhenghong@gmail.com>
2017-04-07 13:46:09 +0800

    refactor

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request 
 from flask_cors import CORS, cross_origin
-import generator
+import email_send
 
 app = Flask(__name__)
 CORS(app)
@@ -11,6 +11,17 @@ def run():
     d = params.get('data')
     p = params.get('letter')
     return(generator.generate_letters(d, p))
+
+@app.route("/send")
+def send():
+    params = request.args
+    print(params)
+    email = params.get('email')
+    pw = params.get('pw')
+    data = params.get('data')+'.csv'
+    body = params.get('body')+'.txt'
+    preview = True if params.get('preview') == 'true' else False 
+    return(str(email_send.main(email, pw , data, body, 0, preview=preview)[0]))
 
 if __name__ == "__main__":
     app.run()
