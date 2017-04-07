bfee86c8583cf875e89b9e63c4956a219c5043b4
lieuzhenghong <lieuzhenghong@gmail.com>
2017-04-07 18:01:03 +0800

    Let client display errors

--- server.py
+++ server.py
@@ -21,17 +21,21 @@ def send():
     body = params.get('body')+'.txt'
     preview = True if params.get('preview') == 'true' else False 
     data = sendr.main(email, pw , data, body, preview=preview)
+    
     payload = []
-    for msg in data:
-        print(msg)
-        output = {}
-        output['subject'] = msg[0][1][1]
-        output['from'] = msg[0][2][1]
-        output['to'] = msg[0][3][1]
-        output['cc'] = msg[0][4][1]
-        output['body'] = msg[1]
-        payload.append(output)
-    return(jsonify(payload))
+    if (type(data) == list):
+        for msg in data:
+            print(msg)
+            output = {}
+            output['subject'] = msg[0][1][1]
+            output['from'] = msg[0][2][1]
+            output['to'] = msg[0][3][1]
+            output['cc'] = msg[0][4][1]
+            output['body'] = msg[1]
+            payload.append(output)
+        return(jsonify(payload))
+    else:
+        return(data)
 
 if __name__ == "__main__":
     app.run()
